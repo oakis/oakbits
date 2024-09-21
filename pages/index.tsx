5dea7e1f5af9c6ba8b30f3bee1c87974a046304b
oakis <oakismen@gmail.com>
2024-09-21 17:39:14 +0200

    render data

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import puppeteer from "puppeteer";
 
 interface Match {
   className: string;
-  innerHTML: string;
   textContent: string | null;
   columns?: string[];
 }
@@ -22,22 +21,27 @@ export const getServerSideProps = async () => {
     { waitUntil: "networkidle2" }
   );
 
-  const rows = await page.evaluate(() =>
-    Array.from(
+  const rows = await page.evaluate(() => {
+    const filterCols = (str: string) =>
+      str.length > 0 &&
+      !str.includes("Omgång") &&
+      !str.includes("Matchfakta") &&
+      str !== " ";
+
+    return Array.from(
       Array.from(document.getElementsByTagName("tbody"))
         .filter((el) => el.role === "rowgroup")
         .toSpliced(0, 3)[0].children
     ).map((row) => ({
       className: row.className,
-      innerHTML: row.innerHTML,
       textContent: row.textContent,
       ...(row.className !== "k-grouping-row" && {
-        columns: Array.from(row.children).map(
-          (child) => child.textContent ?? "-"
-        ),
+        columns: Array.from(row.children)
+          .map((child) => child.textContent ?? "")
+          .filter((str) => filterCols(str.replace(/\u00A0/g, ""))),
       }),
-    }))
-  );
+    }));
+  });
 
   await browser.close();
 
@@ -72,17 +76,30 @@ export default function Page({
   result,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   console.log(result);
+
+  const selectedTeam = "BF Solängen F";
+
   return (
     <main>
-      <table>
+      <table className="border-collapse border border-slate-500">
         {result.map((res) => (
           <tbody key={res.header}>
-            <th>{res.header}</th>
+            <tr className="border border-slate-600">
+              <th>{res.header}</th>
+            </tr>
             {res.matches.map((match) => (
-              <tr
-                key={match.textContent}
-                dangerouslySetInnerHTML={{ __html: match.innerHTML }}
-              ></tr>
+              <tr className="border border-slate-600" key={match.textContent}>
+                {match.columns?.map((col, i) => (
+                  <td
+                    className={
+                      col.includes(selectedTeam) ? "p-5 font-bold" : "p-5"
+                    }
+                    key={`${match.textContent}-${i}`}
+                  >
+                    {col}
+                  </td>
+                ))}
+              </tr>
             ))}
           </tbody>
         ))}
