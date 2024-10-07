e1ee174b50ba242a66846ba599939de2fe921a9a
oakis <oakismen@gmail.com>
2024-10-07 20:18:39 +0200

    fix styling

--- components/MatchBoardScores.tsx
+++ components/MatchBoardScores.tsx
@@ -16,17 +16,19 @@ const MatchBoardScores = ({ scores }: IMatchBoardScores) => {
   return (
     <div className="w-full overflow-auto">
       <div className="flex flex-col">
-        <div className={clsx("flex flex-row")}>
+        <div className="flex flex-row justify-around w-full gap-8 relative">
           <div
-            className="pl-4 flex justify-center items-center"
-            style={{ flex: "0 0 4rem" }}
+            className="flex justify-center items-center font-bold absolute top-0 left-0 w-20"
+            style={{ flex: "0 0 3rem" }}
           >
             Serie
           </div>
+          <div style={{ flex: "0 0 3rem" }} />
+
           {scores.boardNames.map((board, i) => (
             <div
               key={board + i}
-              className="flex flex-row justify-around w-full min-w-72"
+              className="flex flex-row justify-around w-full font-bold min-w-64"
             >
               {board.trim()}
             </div>
@@ -35,19 +37,16 @@ const MatchBoardScores = ({ scores }: IMatchBoardScores) => {
         {scores.series.map((row, rowI) => (
           <div key={"serie" + rowI} className={clsx("flex flex-row")}>
             <div
-              className="pl-4 flex justify-center items-center"
-              style={{ flex: "0 0 4rem" }}
+              className="flex justify-center items-center text-3xl"
+              style={{ flex: "0 0 5rem" }}
             >
               {rowI + 1}
             </div>
-            <div
-              className="flex flex-row justify-around w-full"
-              style={{ marginRight: "-1rem" }}
-            >
+            <div className="flex flex-row justify-around w-full gap-8">
               {row.boards.map((board, boardI) => (
                 <div
                   key={rowI + "row" + boardI}
-                  className={clsx("flex flex-col p-4 w-full min-w-72")}
+                  className={clsx("flex flex-col w-full py-4  min-w-64")}
                 >
                   {board.scores.map((score, scoreI) => (
                     <div
