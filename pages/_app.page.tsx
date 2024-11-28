d638b403f289e937a157aee1102aff24ef4ef590
oakis <oakismen@gmail.com>
2024-11-28 13:24:51 +0100

    feat: add datadog

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,3 +1,4 @@
+import DatadogInit from "@/components/DatadogInit";
 import Navigation from "@/components/Navigation";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Navigation />
       </header>
       <NextNProgress />
+      <DatadogInit />
       <Component {...pageProps} />
     </>
   );
